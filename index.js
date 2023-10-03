ae2247b64483fbfd39faa019ff68b292d845bd2b
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-10-02 21:21:28 -0500

    feature: end of the project 😁

--- index.js
+++ index.js
@@ -10,10 +10,31 @@ const applyOverlayMask = (e) => {
   overlayEL.style = `--opacity:1; --x:${x}px; --y:${y}px;`;
 };
 
-const initOverlayCard = () => {
+const observer = new ResizeObserver((entries) => {
+  entries.forEach((entry) => {
+    const cardIndex = cards.indexOf(entry.target);
+    let width = entry.borderBoxSize[0].inlineSize;
+    let height = entry.borderBoxSize[0].blockSize;
+    if (cardIndex >= 0) {
+      overlay.children[cardIndex].style.width = `${width}px`;
+      overlay.children[cardIndex].style.height = `${height}px`;
+    }
+  });
+});
+
+const createOverlayCta = (overlayCard, ctaEL) => {
+  const overlayCta = document.createElement("div");
+  overlayCta.classList.add("cta");
+  overlayCta.textContent = ctaEL.textContent;
+  overlayCard.append(overlayCta);
+};
+
+const initOverlayCard = (cardEL) => {
   const overlayCard = document.createElement("div");
   overlayCard.classList.add("card");
+  createOverlayCta(overlayCard,cardEL.lastElementChild);
   overlay.append(overlayCard);
+  observer.observe(cardEL);
 };
 
 cards.forEach(initOverlayCard);
